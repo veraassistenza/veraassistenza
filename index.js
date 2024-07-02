00358bda6bacd28a9f470acba326119751c071ed
hnhsnt <133853548+hnhsnt@users.noreply.github.com>
2024-07-02 22:11:04 +0200

    changes to layout

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 document.getElementById('learn-more').addEventListener('click', function() {
     alert('More information will be available soon!');
 });
+
+window.addEventListener('scroll', function() {
+    const scrollPosition = window.scrollY;
+    const rotationFactor = 600; // rate at which the gradient rotates
+    /* const degree = (135 + scrollPosition * rotationFactor) % 360; // Start from 135 degrees */
+    document.body.style.background = `linear-gradient(${degree}deg, white, red)`;
+});
