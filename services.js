e640b804cdd271d83f4dd21b7f57a915e07862bf
ACIS <patronato.cafmi@gmail.com>
2024-07-03 18:58:32 +0200

    made some changes and added a popup on the services page.

--- services.js
+++ services.js
@@ -1,11 +1,27 @@
-window.addEventListener('scroll', function() {
+/* window.addEventListener('scroll', function() {
     const scrollPosition = window.scrollY;
     const rotationFactor = 600; // rate at which the gradient rotates
-    /* const degree = (135 + scrollPosition * rotationFactor) % 360; // Start from 135 degrees */
+    const degree = (135 + scrollPosition * rotationFactor) % 360; // Start from 135 degrees
     document.body.style.background = `linear-gradient(${degree}deg, white, red)`;
+}); */
+
+
+document.getElementById('openPopupImm').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'block';
 });
 
-document.getElementById('learn-more').addEventListener('click', function() {
-    alert('More information will be available soon!');
+document.getElementById('closePopupBtn').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'none';
 });
 
+function showRequirements(element, serviceName, requirements) {
+    var listItems = document.querySelectorAll('.services li');
+    listItems.forEach(item => item.classList.remove('selected'));
+
+    element.classList.add('selected');
+
+    var requirementsDiv = document.getElementById('requirements');
+    requirementsDiv.innerHTML = `<h2>${serviceName}</h2>
+    <p>Requirements:</p>
+    <ul>${requirements.map(req => `<li>${req}</li>`).join('')}</ul>`;
+}
