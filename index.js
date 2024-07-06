8fa74eaa994ff8fd2abfd1aa9c6995cdabc8a5ae
hnhsnt <133853548+hnhsnt@users.noreply.github.com>
2024-07-07 00:02:49 +0200

    Changed the rotation rate of the background gradient.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
     const scrollPosition = window.scrollY;
-    const rotationFactor = 0.15; // rate at which the gradient rotates
+    const rotationFactor = 0.17; // rate at which the gradient rotates
     const degree = (135 + scrollPosition * rotationFactor) % 360; // Start from 135 degrees
     document.body.style.background = `linear-gradient(${degree}deg, white, red)`;
     document.body.style.backgroundAttachment = 'fixed';
