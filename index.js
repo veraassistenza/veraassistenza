d6b01976e7625e62a1a8d4a1de96222090ca05dd
ACIS <patronato.cafmi@gmail.com>
2024-07-18 12:41:29 +0200

    Changed the background.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-window.addEventListener('scroll', function() {
+/* window.addEventListener('scroll', function() {
     const scrollPosition = window.scrollY;
     const rotationFactor = 0.17; // rate at which the gradient rotates
     const degree = (135 + scrollPosition * rotationFactor) % 360; // Start from 135 degrees
     document.body.style.background = `linear-gradient(${degree}deg, #d09393 20%, #da1313 80%)`;
     document.body.style.backgroundAttachment = 'fixed';
-});
+}); */
 
 /* Loading screen start */
 document.addEventListener('DOMContentLoaded', function() {
