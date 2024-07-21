3028b54797548f423b1e9bdd2f58954cd57828a3
hnhsnt <133853548+hnhsnt@users.noreply.github.com>
2024-07-21 17:09:16 +0200

    Made another prototype (to be finished).

---
+++ indextoo.js
@@ -0,0 +1,43 @@
+/* Header start */
+
+    /* Hamburger icon start */
+    function toggleMobileMenu(menu) {
+        menu.classList.toggle('open');
+        document.querySelector('div .mobile-menu-container').classList.toggle('open');
+    }
+    /* Hamburger icon end */
+
+/* Header end */
+
+/* Initial text start */
+/* Text reveal effect on scroll */
+let paragraphs = [...document.querySelectorAll('.initial-text p')];
+let spans = [];
+
+paragraphs.forEach(paragraph => {
+    let htmlString = '';
+    let pArray = paragraph.textContent.split('');
+    for (let i = 0; i < pArray.length; i++) {
+        htmlString += `<span>${pArray[i]}</span>`;
+    }
+    paragraph.innerHTML = htmlString;
+})
+
+spans = [...document.querySelectorAll('span')];
+
+function revealSpans(){
+    for (let i = 0; spans.length; i++) {
+        if (spans[i].parentElement.getBoundingClientRect().top < window.innerHeight / 2) {
+            let {left, top} = spans[i].getBoundingClientRect();
+            top = top - (window.innerHeight * 0.4); /* Adjust to set where the reveal effect starts on the page */
+            let opacityValue = 1 - ((top * 0.01) + (left * 0.001)) < 0.1 ? 0.1 : 1 - ((top * 0.01) + (left * 0.001)).toFixed(3);
+            opacityValue = opacityValue > 1 ? 1 : opacityValue.toFixed(3);
+            spans[i].style.opacity = opacityValue;
+        }
+    }
+}
+
+window.addEventListener('scroll', () => {
+    revealSpans();
+})
+/* Initial text end */
