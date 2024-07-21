b7b1263b67960732c8e838b960e883819a783a55
hnhsnt <133853548+hnhsnt@users.noreply.github.com>
2024-07-21 22:50:40 +0200

    Added services and contacts section. Services accordion to be added soon.

--- indextoo.js
+++ indextoo.js
@@ -1,4 +1,18 @@
 /* Header start */
+/* Header visibility on scroll */
+let lastScrollTop = 0;
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        // Downscroll
+        header.classList.add('header--hidden');
+    } else if (scrollTop === 0) {
+        header.classList.remove('header--hidden');
+    }
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
+});
 
     /* Hamburger icon start */
     function toggleMobileMenu(menu) {
@@ -6,7 +20,6 @@
         document.querySelector('div .mobile-menu-container').classList.toggle('open');
     }
     /* Hamburger icon end */
-
 /* Header end */
 
 /* Initial text start */
