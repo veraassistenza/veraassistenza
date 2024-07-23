96bf0c2e683d97e967b52c2cd27bbcd2b5e2fa19
ACIS <patronato.cafmi@gmail.com>
2024-07-23 13:35:26 +0200

    Fixed annoying non-responsiveness on services text part

--- indextoo.js
+++ indextoo.js
@@ -59,12 +59,10 @@ window.addEventListener('scroll', () => {
 let acc_boxEl = document.querySelectorAll(".acc-box");
 let symbol = document.querySelector(".symbol");
 
+
 acc_boxEl.forEach((item, index) => {
     item.addEventListener("click", () => {
-        acc_boxEl.forEach(eachItem => {
-            eachItem.classList.remove("active");
-        })
-        item.classList.add("active");
+        item.classList.toggle("active");
     })
 })
 
