a25caaa7231c0055a469e55dbc8acc0aefb29f01
veraassistenza <patronato.cafmi@gmail.com>
2024-07-27 17:56:06 +0200

    Made services-text's h2 smaller on mobile devices so that it doesn't have horizontal scroll. Changed beginning animation.

--- indextoo.js
+++ indextoo.js
@@ -83,3 +83,35 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el))
 /* Extras: scroll animations end */
+
+/* Random letters animation */
+/* Random letters animation */
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+document.addEventListener('DOMContentLoaded', () => {
+    let veraParagraphs = [...document.querySelectorAll('.main-text p[data-value]')];
+
+    veraParagraphs.forEach(paragraph => {
+        let iteration = 0;
+        const originalText = paragraph.dataset.value;
+
+        const interval = setInterval(() => {
+            paragraph.innerText = paragraph.innerText
+                .split("")
+                .map((letter, index) => {
+                    if (index < iteration) {
+                        return originalText[index];
+                    }
+
+                    return letters[Math.floor(Math.random() * 26)];
+                })
+                .join("");
+
+            if (iteration >= originalText.length) {
+                clearInterval(interval);
+            }
+
+            iteration += 1 / 3;
+        }, 30);
+    });
+});
